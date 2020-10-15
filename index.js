4af58f375359a6d581c6267905ca6974b70e2b04
OUALI YANNICK <yo@mydatamodels.com>
2020-10-15 18:57:37 +0200

    allow cross origin from cors

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
