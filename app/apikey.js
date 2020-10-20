f47b89bc1254b3fe20e2dadd8013814b381ebc2a
OUALI YANNICK <yo@mydatamodels.com>
2020-10-20 19:48:44 +0200

    shortcut apikey method export.

--- app/apikey.js
+++ app/apikey.js
@@ -1,8 +1,6 @@
 const db = require('./schema/models')
 
-const API_KEY = async () => {
+module.exports = async () => {
     const { riotKey } = await db.serverConfig.findOne({ type: 'APIKEY' })
     return riotKey
 }
-
-module.exports = API_KEY
