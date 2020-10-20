b85d9c362b80f42dcb2d9e28c306288b48986f84
OUALI YANNICK <yo@mydatamodels.com>
2020-10-20 20:02:15 +0200

    reset cors

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept'
